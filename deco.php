bf92b2aa00904ddfd45c4347d93c149abbf22fd3
System Administrator <root@macbook-pro-de-cedric.home>
2015-10-31 12:38:58 +0100

    initial commit

--- deco.php
+++ deco.php
@@ -1,6 +1,6 @@
 <?php 
 try{
-	$bdd = new PDO('mysql:host=localhost;dbname=Stream_Audio','root','');
+	$bdd = new PDO('mysql:host=localhost;dbname=Stream_Audio','root','Az3rty');
 	$bdd->setAttribute(PDO::ATTR_CASE,PDO::CASE_LOWER);
 	$bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	$bdd->exec("SET NAMES 'utf8'");
