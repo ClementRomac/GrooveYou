962de8142f464cac76a7028c99bab37cc64f77d9
clément romac <clment romac>
2015-12-20 22:52:52 +0100

    connection bdd hostinger

--- deco.php
+++ deco.php
@@ -1,6 +1,6 @@
 <?php 
 try{
-	$bdd = new PDO('mysql:host=localhost;dbname=u966249616_1','root','X3tdhU0WTi');
+	$bdd = new PDO('mysql:host=mysql.hostinger.fr;dbname=u622336288_1','u622336288_admin','oXJUYg13ib');
 	$bdd->setAttribute(PDO::ATTR_CASE,PDO::CASE_LOWER);
 	$bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	$bdd->exec("SET NAMES 'utf8'");
