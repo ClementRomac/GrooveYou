478511e021660a2d635c2ade6ab7e2a6bfcb3f91
clément romac <clment romac>
2015-12-21 11:10:53 +0100

    correction bug affichage connectes

--- streamers.php
+++ streamers.php
@@ -12,7 +12,7 @@ if(isset($_SESSION['username'])){
 	}
 
 	//AFFICHAGE NOMBRE UTILISATEURS CONNECTES
-	$retour = $bdd->prepare('SELECT username, link FROM users WHERE username != :username AND link != "" AND ip != "0"');
+	$retour = $bdd->prepare('SELECT username, link FROM users WHERE username != :username AND ip != ""');
 	$retour->execute(array('username' => $_SESSION['username']));
 	$donnees = $retour->fetchAll();
 	echo "<p> Il y a ". count($donnees);
@@ -20,7 +20,9 @@ if(isset($_SESSION['username'])){
 	
 	// RECUPERATION DES STREAMEURS
 	foreach ($donnees as $value) {
-		echo "<label><input type='radio' name='streamers_tolisten' value='".$value['link']."'>".$value['username']."</label><br>";
+		if($value['link'] != ""){
+			echo "<label><input type='radio' name='streamers_tolisten' value='".$value['link']."'>".$value['username']."</label><br>";
+		}
 	}
 	if(count($donnees)>0)
 		echo "<input type='submit' value='Lire'>";
