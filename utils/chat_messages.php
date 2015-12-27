2edb0d0f46e5d1ef6bc5d1dbd665577de70cc8c1
clément romac <clment romac>
2015-12-27 13:19:41 +0100

    ajout limit 20 aux messages du chat

--- utils/chat_messages.php
+++ utils/chat_messages.php
@@ -4,7 +4,7 @@ require_once("bdd.php");
 require_once("connection.js");
 
 if(isset($_SESSION['username'])){
-	$query = $bdd->prepare('SELECT id, username, message, time FROM chat WHERE room = :room ORDER BY id DESC');
+	$query = $bdd->prepare('SELECT id, username, message, time FROM chat WHERE room = :room ORDER BY id DESC LIMIT 20');
 	$query->execute(array('room' => $_SESSION['room']));
 	while ($data = $query->fetch()) {
 		echo "<p>>>".$data['username']." - ".$data['time']." > ".$data['message']."</p>";
