c8d85c9d54ff701ca9e074e899791e140dbf7df9
clément romac <clment romac>
2016-02-17 15:28:15 +0100

    ajout api

---
+++ API_LPC.php
@@ -0,0 +1,17 @@
+<?php
+try{
+	$bdd = new PDO('mysql:host=mysql.hostinger.fr;dbname=u622336288_lpc','u622336288_root','administrateur');
+	$bdd->setAttribute(PDO::ATTR_CASE,PDO::CASE_LOWER);
+	$bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	$bdd->exec("SET NAMES 'utf8'");
+}
+catch(PDOException $e){
+	die("Erreur MySQL: ".$e->getMessage());
+}
+
+if(!empty($_GET['infos_parents'])){
+	$query = $bdd->execute("SELECT * FROM infos_parents");
+	$response = $query->fetch();
+	header('Content-Type: application/json');
+	echo json_encode($response);
+}
