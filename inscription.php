915a43e1ad6a70418c04ba3133f531b81434b47b
clément romac <clment romac>
2016-02-17 14:56:45 +0100

    ajout password inscription

--- inscription.php
+++ inscription.php
@@ -21,28 +21,34 @@ session_start();
 <?php
 if(empty($_SESSION['username'])){
 	echo "<form method='POST' action=''>
+			<input type='text' name='passwd_groovyou'><br>
 			<input type='text' name='username' placeholder='Pseudo'></br>	
 			<input type='password' name='password' placeholder='Mot de passe'></br>
 			<input type='submit' value=\"S'Inscrire ! \" name='inscription'></br>
 		</form><br><br>
 	<a href='index.php'>Retour</a>";
 	if(isset($_POST['inscription'])){
-		if(!empty($_POST['username']) && !empty($_POST['password'])){
-			$query = $bdd->prepare('SELECT username FROM users WHERE username = :username');
-			$query->execute(array('username' => $_POST['username']));
-			$is_already_connected = $query->fetch();
-			if(empty($is_already_connected['username'])){
-				$query = $bdd->prepare("INSERT INTO users SET username = :username, password = :password, link = '', ip = '', time = ''");
-				$query->execute(array('username' => $_POST['username'],
-										'password' => md5($_POST['password'])));
-				header('Location: index.php');
+		if($_POST['passwd_groovyou'] == "tractopelledu33"){
+			if(!empty($_POST['username']) && !empty($_POST['password'])){
+				$query = $bdd->prepare('SELECT username FROM users WHERE username = :username');
+				$query->execute(array('username' => $_POST['username']));
+				$is_already_connected = $query->fetch();
+				if(empty($is_already_connected['username'])){
+					$query = $bdd->prepare("INSERT INTO users SET username = :username, password = :password, link = '', ip = '', time = ''");
+					$query->execute(array('username' => $_POST['username'],
+											'password' => md5($_POST['password'])));
+					header('Location: index.php');
+				}
+				else{
+					echo "Ce pseudo existe déjà !";
+				}
 			}
 			else{
-				echo "Ce pseudo existe déjà !";
+				echo "Veuillez remplir tous les champs !";
 			}
 		}
 		else{
-			echo "Veuillez remplir tous les champs !";
+			echo "Accès interdit !";
 		}
 	}
 }
